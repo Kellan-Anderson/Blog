908de328256ba1c3d148921b290cd55237547940
Kellan Anderson <kellandanderson@gmail.com>
2023-06-17 20:11:16 -0400

    Init commit, first migration

---
+++ src/db/schema.ts
@@ -0,0 +1,8 @@
+import { sql } from 'drizzle-orm'
+import { pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core'
+
+export const blog = pgTable('blog', {
+  id: serial('id').primaryKey(),
+  created_at: timestamp('created_at').defaultNow(),
+  content: text('content')
+});
